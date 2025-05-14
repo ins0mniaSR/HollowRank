0c58dec1d20451a4a60dcfa8251fed0c9b1c2bc0
ins0mniaSR <mjpaull99@bigpond.com>
2025-05-15 08:13:15 +0930

    Created basic command line input system to only grab the desired boards and generate their respective .csv outputs. .csv generation is now done with a dict, increasing future scalability

--- config.py
+++ config.py
@@ -2,22 +2,24 @@
 mainScoreMax = 100000 # Default 100000
 mainDecayMod = 0.5    # How quickly added runs' value decreases, defualt 0.5
 
-nmgMainActiveNum = 20 # How many runs are needed to remove all penalties for a low run count on an NMG main board, default 20
-glitchedMainActiveNum = 10 # how many runs are needed to remove all penalties for a low run count on a glitched main board, default 10
+nmgMainActiveNum = 100 # How many runs are needed to remove all penalties for a low run count on an NMG main board, default 20
+glitchedMainActiveNum = 20 # how many runs are needed to remove all penalties for a low run count on a glitched main board, default 10
 
 ### Extension Boards
 extensionScoreMax = 50000 # Default 50000
 extensionDecayMod = 0.5    # How quickly added runs' value decreases, defualt 0.5
 
-nmgExtensionActiveNum = 20 # How many runs are needed to remove all penalties for a low run count on an NMG main board, default 20
-glitchedExtensionActiveNum = 10 # how many runs are needed to remove all penalties for a low run count on a glitched main board, default 10
+nmgExtensionActiveNum = 50 # How many runs are needed to remove all penalties for a low run count on an NMG main board, default 20
+glitchedExtensionActiveNum = 20 # how many runs are needed to remove all penalties for a low run count on a glitched main board, default 10
 
 
 ### Export Filenames
-nmgMainOutput = "nmgMainOutput.csv"
-glitchedMainOutput = "glitchedMainOutput.csv"
-nmgExtensionsOutput = "nmgExtensionsOutput.csv"
-glitchedExtensionsOutput = "glitchedExtensionsOutput.csv"
-totalNmgOutput = "totalNmgOutput.csv"
-totalGlitchedOutput = "totalGlitchedOutput.csv"
-totalOutput = "totalOutput.csv"
+exportFilenames = {
+"nmgMain" : "nmgMainOutput.csv",
+"glitchedMain" : "glitchedMainOutput.csv",
+"nmgExtension" : "nmgExtensionsOutput.csv",
+"glitchedExtension" : "glitchedExtensionsOutput.csv",
+"nmgTotal" : "totalNmgOutput.csv",
+"glitchedTotal" : "totalGlitchedOutput.csv",
+"total" : "totalOutput.csv"
+}
--- main.py
+++ main.py
@@ -1,45 +1,70 @@
-from srcVars import nmgMainCatIdDict, glitchedMainCatIdDict, nmgExtensionCatIdDict, glitchedExtensionCatIdDict, mainGameId, extensionGameId #add glitched main board calcs @ins0mniaSR
+### TODO Add ILs, Console Runs, Modded. Tweak scoring, possibly raise min activity thresholds for mainboard to 50?
+
+from srcVars import nmgMainCatIdDict, glitchedMainCatIdDict, nmgExtensionCatIdDict, glitchedExtensionCatIdDict, mainGameId, extensionGameId 
 from csvExporter import csvExport
 import config
 from boardScoringHandler import boardHandler
 
 sortedScoreDict = {}
+finishedFetching = 0
+outputCsv = 0
+boardsFetched = {"nmgMain" : False, "glitchedMain" : False, "nmgExtensions" : False, "glitchedExtensions" : False, "nmgTotal" : False, "glitchedTotal" : False}
+while finishedFetching == 0:
+    userIn = input("Select which boards you want to fetch data for: \n 1: NMG Mainboard \n 2: Glitched Main \n 3: NMG Extensions \n 4: Glitched Extensions \n")
+    if userIn == "1": # NMG Main Boards
+        print("Fetching NMG Main Boards...")
+        sortedScoreDict["nmgMain"] = boardHandler(mainGameId, nmgMainCatIdDict, config.mainScoreMax, config.nmgMainActiveNum, config.mainDecayMod)
+        boardsFetched["nmgMain"] = True
+        print("Fetched NMG Main Boards!\n")
+    elif userIn == "2": # Glitched Main Boards
+        print("Fetching Glitched Main Boards...")
+        sortedScoreDict["glitchedMain"] = boardHandler(mainGameId, glitchedMainCatIdDict, config.mainScoreMax, config.glitchedMainActiveNum, config.mainDecayMod)
+        boardsFetched["glitchedMain"] = True
+        print("Fetched Glitched Main Boards!\n")
+    elif userIn == "3": # NMG Extension Boards
+        print("Fetching NMG Extension Boards...")
+        sortedScoreDict["nmgExtension"] = boardHandler(extensionGameId, nmgExtensionCatIdDict, config.extensionScoreMax, config.nmgExtensionActiveNum, config.extensionDecayMod)
+        boardsFetched["nmgExtensions"] = True
+        print("Fetched NMG Extension Boards!\n")
+    elif userIn == "4":
+        print("Fetching Glitched Extension Boards...") # Glitched Extension Boards
+        sortedScoreDict["glitchedExtension"] = boardHandler(extensionGameId, glitchedExtensionCatIdDict, config.extensionScoreMax, config.glitchedExtensionActiveNum, config.extensionDecayMod)
+        boardsFetched["glitchedExtensions"] = True
+        print("Fetched Glitched Extension Boards!\n")
+    else:
+        print("Invalid input\n")
+    userIn = input("Are you finished fetching boards? (Y to proceed)\n")
+    if userIn == "Y":
+        finishedFetching = 1
 
-# Generate score totals
-sortedScoreDict["nmgMain"] = boardHandler(mainGameId, nmgMainCatIdDict, config.mainScoreMax, config.nmgMainActiveNum, config.mainDecayMod) # NMG Main Boards
-sortedScoreDict["glitchedMain"] = boardHandler(mainGameId, glitchedMainCatIdDict, config.mainScoreMax, config.glitchedMainActiveNum, config.mainDecayMod) # Glitched Main Boards
-sortedScoreDict["nmgExtension"] = boardHandler(extensionGameId, nmgExtensionCatIdDict, config.extensionScoreMax, config.nmgExtensionActiveNum, config.extensionDecayMod) # NMG Extension Boards
-sortedScoreDict["glitchedExtension"] = boardHandler(extensionGameId, glitchedExtensionCatIdDict, config.extensionScoreMax, config.glitchedExtensionActiveNum, config.extensionDecayMod) # Glitched Main Boards
+# Make summed total scores for NMG / Glitched / Overall
+print("Calculating applicable combined scores...")
+if boardsFetched["nmgMain"] == True and boardsFetched["nmgExtensions"] == True: # Combined NMG board if both were fetched
+    boardsFetched["nmgTotal"] = True
+    nmgCats = ["nmgMain", "nmgExtension"]
+    totalNmgScoreDict = {}
+    for category in nmgCats:
+        for name in sortedScoreDict[category]:
+            totalNmgScoreDict[name] = totalNmgScoreDict.setdefault(name, 0) + sortedScoreDict[category][name]
+    sortedScoreDict["nmgTotal"] = dict(sorted(totalNmgScoreDict.items(), key=lambda item: item[1], reverse=True))
+if boardsFetched["glitchedMain"] == True and boardsFetched["glitchedExtensions"] == True: # Combined Glitched board if both were fetched
+    boardsFetched["glitchedTotal"] = True
+    glitchedCats = ["glitchedMain", "glitchedExtension"]
+    totalGlitchedScoreDict = {}
+    for category in glitchedCats:
+        for name in sortedScoreDict[category]:
+            totalGlitchedScoreDict[name] = totalGlitchedScoreDict.setdefault(name, 0) + sortedScoreDict[category][name]
+    sortedScoreDict["glitchedTotal"] = dict(sorted(totalGlitchedScoreDict.items(), key=lambda item: item[1], reverse=True))
+if boardsFetched["nmgTotal"] == True and boardsFetched["glitchedTotal"] == True: #Total board if all were fetched
+    totalScoreDict = {}
+    for category in sortedScoreDict:
+        for name in sortedScoreDict[category]:
+            totalScoreDict[name] = totalScoreDict.setdefault(name, 0) + sortedScoreDict[category][name]
+    sortedScoreDict["total"] = dict(sorted(totalScoreDict.items(), key=lambda item: item[1], reverse=True)) 
 
-# Make summed total scores
-nmgCats = ["nmgMain", "nmgExtension"]
-totalNmgScoreDict = {}
-for category in nmgCats:
-    for name in sortedScoreDict[category]:
-        totalNmgScoreDict[name] = totalNmgScoreDict.setdefault(name, 0) + sortedScoreDict[category][name]
+print("Created applicable combined scores!\nCreating .csv...")
 
-glitchedCats = ["glitchedMain", "glitchedExtension"]
-totalGlitchedScoreDict = {}
-for category in glitchedCats:
-    for name in sortedScoreDict[category]:
-        totalGlitchedScoreDict[name] = totalGlitchedScoreDict.setdefault(name, 0) + sortedScoreDict[category][name]
-
-totalScoreDict = {}
-for category in sortedScoreDict:
-    for name in sortedScoreDict[category]:
-        totalScoreDict[name] = totalScoreDict.setdefault(name, 0) + sortedScoreDict[category][name]
-
-# Now sort it 
-sortedNmgTotalDict = dict(sorted(totalNmgScoreDict.items(), key=lambda item: item[1], reverse=True)) #sort the above
-sortedGlitchedTotalDict = dict(sorted(totalGlitchedScoreDict.items(), key=lambda item: item[1], reverse=True)) 
-sortedTotalDict = dict(sorted(totalScoreDict.items(), key=lambda item: item[1], reverse=True)) 
-
-
-#Export score totals
-csvExport(sortedScoreDict["nmgMain"], config.nmgMainOutput) # Export NMG Main
-csvExport(sortedScoreDict["glitchedMain"], config.glitchedMainOutput) # Export Glitched Main
-csvExport(sortedScoreDict["nmgExtension"], config.nmgExtensionsOutput) # Export NMG Extensions
-csvExport(sortedScoreDict["glitchedExtension"], config.glitchedExtensionsOutput) # Export Glitched Extensions
-csvExport(sortedNmgTotalDict, config.totalNmgOutput) # Export NMG Sums
-csvExport(sortedGlitchedTotalDict, config.totalGlitchedOutput) # Export Glitched Sums
-csvExport(sortedTotalDict, config.totalOutput) # Export Sums
+#Export score totals for data that has been generated, to their respective .csv file
+for exportBoard in sortedScoreDict:
+    csvExport(sortedScoreDict[exportBoard], config.exportFilenames[exportBoard])
+print("\nComplete!")
