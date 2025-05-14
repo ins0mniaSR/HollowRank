02f20e6b217462a01f6529078ed1b1d26239e1ad
ins0mniaSR <mjpaull99@bigpond.com>
2025-05-14 12:49:35 +0930

    Added total calculation and output

--- main.py
+++ main.py
@@ -11,8 +11,19 @@ sortedScoreDict["glitchedMain"] = boardHandler(mainGameId, glitchedMainCatIdDict
 sortedScoreDict["nmgExtension"] = boardHandler(extensionGameId, nmgExtensionCatIdDict, config.extensionScoreMax, config.nmgExtensionActiveNum, config.extensionDecayMod) # NMG Extension Boards
 sortedScoreDict["glitchedExtension"] = boardHandler(extensionGameId, glitchedExtensionCatIdDict, config.extensionScoreMax, config.glitchedExtensionActiveNum, config.extensionDecayMod) # Glitched Main Boards
 
+# Make a summed total score
+totalScoreDict = {}
+for category in sortedScoreDict:
+    for name in sortedScoreDict[category]:
+        totalScoreDict[name] = totalScoreDict.setdefault(name, 0) + sortedScoreDict[category][name]
+
+# Now sort it 
+sortedTotalDict = dict(sorted(totalScoreDict.items(), key=lambda item: item[1], reverse=True)) #sort the above
+
+
 #Export score totals
 csvExport(sortedScoreDict["nmgMain"], config.nmgMainOutput) # Export NMG Main
 csvExport(sortedScoreDict["glitchedMain"], config.glitchedMainOutput) # Export Glitched Main
 csvExport(sortedScoreDict["nmgExtension"], config.nmgExtensionsOutput) # Export NMG Extensions
 csvExport(sortedScoreDict["glitchedExtension"], config.glitchedExtensionsOutput) # Export Glitched Extensions
+csvExport(sortedTotalDict, config.totalOutput) # Export Sums
